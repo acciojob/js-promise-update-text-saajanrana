a552a55c5120e681385b8d4858e0f240104f5cfc
saajanrana <123057148+saajanrana@users.noreply.github.com>
2023-04-04 21:51:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function getMessage() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
